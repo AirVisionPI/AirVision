b1cfe7198fbb77155bf948b778c4e49f2c6e8897
Alexandre-Gavazzi <89035054+Alexandre-Gavazzi@users.noreply.github.com>
2022-04-19 13:47:13 -0300

    atualizando js login

--- public/javascript/cadastro_login_chumbado.js
+++ public/javascript/cadastro_login_chumbado.js
@@ -1,77 +1,126 @@
-  
-  // CADASTRO DE ADMIN NA TELA INICIAL
-  function btnCadastrar() {
+// CADASTRO DE ADMIN NA TELA INICIAL
+function btnCadastrar() {
+  let formulario = new URLSearchParams(
+    new FormData(document.getElementById("form_cadastro"))
+  );
+  let nome = formulario.get("nome");
+  let email = formulario.get("email");
+  let senha = formulario.get("senha");
+  let razao_social = formulario.get("razao_social");
+  let cnpj = formulario.get("cnpj");
+  let local_companhia = formulario.get("local_companhia");
 
-    let formulario = new URLSearchParams(new FormData(document.getElementById("form_cadastro")));
-    let nome = formulario.get("nome");
-    let email = formulario.get("email");
-    let senha = formulario.get("senha");
-    let razao_social = formulario.get("razao_social");
-    let cnpj = formulario.get("cnpj");
-    let local_companhia = formulario.get("local_companhia");
+  const myBD = JSON.parse(localStorage.getItem("myBD"));
 
-    const myBD = JSON.parse(localStorage.getItem("myBD"));
-
-    if(myBD){
-      localStorage.setItem("myBD", JSON.stringify([...myBD,{
-        id: myBD.at(-1).id + 1,
-        nome, 
-        email, 
-        senha, 
-        razao_social, 
-        cnpj, 
-        local_companhia,
-        cargo: "admin"
-      }]))
-      alert(`Cadastro Efetuado com Sucesso!`);
-    }else{
-      localStorage.setItem("myBD", JSON.stringify([{
-        id: 1, 
-        nome, 
-        email, 
-        senha, 
-        razao_social, 
-        cnpj, 
-        local_companhia, 
-        cargo: "admin"
-      }]))
-      alert(`Cadastro Efetuado com Sucesso!`);
-    }
-    
+  // VALIDAÇÃO
+  if (
+    nome == "" ||
+    email == "" ||
+    senha == "" ||
+    razao_social == "" ||
+    cnpj == "" ||
+    local_companhia == ""
+  ) {
+    alert("Preencha corretamente os dados do fomulário");
+    return;
   }
 
-  // CADASTRO DE FUNCIONARIO E GESTOR PELO ADMIN NA TELA INICIAL
-  function btnCadastrarFunc() {
-
-    let formulario = new URLSearchParams(new FormData(document.getElementById("form_funcionario")));
-    let nome = formulario.get("nome");
-    let email = formulario.get("email");
-    let senha = formulario.get("senha");
-    let aeroporto_trabalho = formulario.get("aeroporto_trabalho");
-    let cargo = formulario.get("cargo");
-
-    const myBD = JSON.parse(localStorage.getItem("myBD"));
-
-    if(myBD){
-      localStorage.setItem("myBD", JSON.stringify([...myBD,{
-        id: myBD.at(-1).id + 1,
-        nome,
-        email,
-        senha,
-        aeroporto_trabalho,
-        cargo,
-      }]))
-      alert(`Cadastro Efetuado com Sucesso!`);
-    }else{
-      localStorage.setItem("myBD", JSON.stringify([{
-        id: 1,
-        nome,
-        email,
-        senha,
-        aeroporto_trabalho,
-        cargo,
-      }]))
-      alert(`Cadastro Efetuado com Sucesso!`);
-    }
-    
+  // ESSA PARTE FAZ O CADASTRO NO LOCAL STORAGE
+  if (myBD) {
+    localStorage.setItem(
+      "myBD",
+      JSON.stringify([
+        ...myBD,
+        {
+          id: myBD.at(-1).id + 1,
+          nome,
+          email,
+          senha,
+          razao_social,
+          cnpj,
+          local_companhia,
+          cargo: "admin",
+        },
+      ])
+    );
+    alert(`Cadastro Efetuado com Sucesso!`);
+  } else {
+    localStorage.setItem(
+      "myBD",
+      JSON.stringify([
+        {
+          id: 1,
+          nome,
+          email,
+          senha,
+          razao_social,
+          cnpj,
+          local_companhia,
+          cargo: "admin",
+        },
+      ])
+    );
+    alert(`Cadastro Efetuado com Sucesso!`);
   }
+}
+
+// CADASTRO DE FUNCIONARIO E GESTOR PELO ADMIN NA TELA INICIAL
+function btnCadastrarFunc() {
+  let formulario = new URLSearchParams(
+    new FormData(document.getElementById("form_funcionario"))
+  );
+  let nome = formulario.get("nome");
+  let email = formulario.get("email");
+  let senha = formulario.get("senha");
+  let aeroporto_trabalho = formulario.get("aeroporto_trabalho");
+  let cargo = formulario.get("cargo");
+
+  const myBD = JSON.parse(localStorage.getItem("myBD"));
+
+  // VALIDAÇÃO
+  if (
+    nome == "" ||
+    email == "" ||
+    senha == "" ||
+    razao_social == "" ||
+    cnpj == "" ||
+    local_companhia == ""
+  ) {
+    alert("Preencha corretamente os dados do fomulário");
+    return;
+  }
+
+  // ESSA PARTE FAZ O CADASTRO NO LOCAL STORAGE
+  if (myBD) {
+    localStorage.setItem(
+      "myBD",
+      JSON.stringify([
+        ...myBD,
+        {
+          id: myBD.at(-1).id + 1,
+          nome,
+          email,
+          senha,
+          aeroporto_trabalho,
+          cargo,
+        },
+      ])
+    );
+    alert(`Cadastro Efetuado com Sucesso!`);
+  } else {
+    localStorage.setItem(
+      "myBD",
+      JSON.stringify([
+        {
+          id: 1,
+          nome,
+          email,
+          senha,
+          aeroporto_trabalho,
+          cargo,
+        },
+      ])
+    );
+    alert(`Cadastro Efetuado com Sucesso!`);
+  }
+}
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,19 +1,26 @@
 function btnEntrar() {
-
-  var formulario = new URLSearchParams(new FormData(document.getElementById("form_login")));
+  var formulario = new URLSearchParams(
+    new FormData(document.getElementById("form_login"))
+  );
   var email = formulario.get("email");
   var senha = formulario.get("senha");
 
-  const myBD = JSON.parse(localStorage.getItem("myBD"));
+  try {
+    const myBD = JSON.parse(localStorage.getItem("myBD"));
 
-  const verificacaoLogin = myBD.find(registro => registro.email === email && registro.senha === senha);
+    const verificacaoLogin = myBD.find(
+      (registro) => registro.email === email && registro.senha === senha
+    );
 
-  if(verificacaoLogin) {
-    alert(`Bem Vindo ${verificacaoLogin.nome}`);
-    sessionStorage.email = verificacaoLogin.email;
-    sessionStorage.cargo = verificacaoLogin.cargo;
-    window.location = "./Dashboard/monitoramento.html";
-  } else {
+    if (verificacaoLogin) {
+      alert(`Bem Vindo ${verificacaoLogin.nome}`);
+      sessionStorage.email = verificacaoLogin.email;
+      sessionStorage.cargo = verificacaoLogin.cargo;
+      window.location = "./Dashboard/monitoramento.html";
+    } else {
+      alert(`Cadastro Inexistente`);
+    }
+  } catch (error) {
     alert(`Cadastro Inexistente`);
   }
 }
