4c93e27987e6dec9bcae767359c5ca5f6b24dfb1
Alexandre-Gavazzi <89035054+Alexandre-Gavazzi@users.noreply.github.com>
2022-04-25 18:36:59 -0300

    teste Azure 1.4

--- aplicacao_web/src/models/usuarioModel.js
+++ aplicacao_web/src/models/usuarioModel.js
@@ -31,8 +31,9 @@ function cadastrar(nome, email, senha, razao_social, cnpj, local_companhia) {
 
     var instrucao_usuario = `
     INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario, cargo_usuario, fk_aeroporto) VALUES 
-    ('${nome}', '${email}', '${senha}', 'admin', (SELECT id_aeroporto FROM companhia_aerea WHERE razao_aeroporto = '${razao_social}' AND localidade_aeroporto = '${local_companhia}' AND cnpj_aeroporto = '${cnpj}'));
+    ('${nome}', '${email}', '${senha}', 'admin', '2');
     `;
+    // (SELECT id_aeroporto FROM companhia_aerea WHERE razao_aeroporto = '${razao_social}' AND localidade_aeroporto = '${local_companhia}' AND cnpj_aeroporto = '${cnpj}')
     console.log("Executando a instrução SQL: \n" + instrucao_usuario);
 
     var instrucao_companhia = `
