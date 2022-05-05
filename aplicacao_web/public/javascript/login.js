360fd8c8c4fd340eeabbd63fa000ad860e7bd9ea
ALlan-Batista <Allan.lima@bandtec.com.br>
2022-05-05 14:25:21 -0300

    Responsividade Login

--- aplicacao_web/public/javascript/login.js
+++ aplicacao_web/public/javascript/login.js
@@ -20,49 +20,48 @@ function btnEntrar() {
 
   fetch("/usuarios/autenticar", {
     method: "POST",
-    body: formulario
-  }).then(function (resposta) {
-    console.log("ESTOU NO THEN DO entrar()!")
-
-    if (resposta.ok) {
-      if (resposta.length == 0) {
-        window.alert('Cadastro incorreto ou inexistente');
-      }
-      console.log(resposta);
-
-      resposta.json().then(json => {
-        console.log(json);
-        console.log(JSON.stringify(json));
-
-        sessionStorage.email = json.email_usuario;
-        sessionStorage.nome = json.nome_usuario;
-        sessionStorage.cargo = json.cargo_usuario;
-        sessionStorage.id = json.id_usuario;
-        sessionStorage.idAeroporto = json.id_aeroporto;
-        sessionStorage.razaoAeroporto = json.razao_aeroporto;
-        sessionStorage.cnpjAeroporto = json.cnpj_aeroporto;
-        sessionStorage.responsavelAeroporto = json.responsavel_aeroporto;
-        sessionStorage.localidadeAeroporto = json.localidade_aeroporto;
-
-        setTimeout(function () {
-          window.alert(`Seja bem vindo ${sessionStorage.nome}!`);
-          window.location = "./Dashboard/monitoramento.html";
-        }, 500); // apenas para exibir o loading
-
-      });
-
-    } else {
-      window.alert('Cadastro incorreto ou inexistente!');
-      console.log("Houve um erro ao tentar realizar o login!");
-
-      resposta.text().then(texto => {
-        console.error(texto);
-      });
-    }
-
-  }).catch(function (erro) {
-    console.log(erro);
+    body: formulario,
   })
+    .then(function (resposta) {
+      console.log("ESTOU NO THEN DO entrar()!");
+
+      if (resposta.ok) {
+        if (resposta.length == 0) {
+          window.alert("Cadastro incorreto ou inexistente");
+        }
+        console.log(resposta);
+
+        resposta.json().then((json) => {
+          console.log(json);
+          console.log(JSON.stringify(json));
+
+          sessionStorage.email = json.email_usuario;
+          sessionStorage.nome = json.nome_usuario;
+          sessionStorage.cargo = json.cargo_usuario;
+          sessionStorage.id = json.id_usuario;
+          sessionStorage.idAeroporto = json.id_aeroporto;
+          sessionStorage.razaoAeroporto = json.razao_aeroporto;
+          sessionStorage.cnpjAeroporto = json.cnpj_aeroporto;
+          sessionStorage.responsavelAeroporto = json.responsavel_aeroporto;
+          sessionStorage.localidadeAeroporto = json.localidade_aeroporto;
+
+          setTimeout(function () {
+            window.alert(`Seja bem vindo ${sessionStorage.nome}!`);
+            window.location = "./Dashboard/monitoramento.html";
+          }, 500); // apenas para exibir o loading
+        });
+      } else {
+        window.alert("Cadastro incorreto ou inexistente!");
+        console.log("Houve um erro ao tentar realizar o login!");
+
+        resposta.text().then((texto) => {
+          console.error(texto);
+        });
+      }
+    })
+    .catch(function (erro) {
+      console.log(erro);
+    });
 
   return false;
 
